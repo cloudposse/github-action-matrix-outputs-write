b087b3e95b75c4c2295f9df99b6f9fc8d150055d
Igor Rodionov <goruha@gmail.com>
2023-04-14 11:26:11 +0300

    Fix special chars (#17)
    ## what
    * Fix special chars issue

---
+++ index.js
@@ -0,0 +1,89 @@
+const core = require('@actions/core');
+const yaml = require('yaml')
+const artifact = require('@actions/artifact');
+const crypto = require('crypto');
+const fs = require('fs');
+
+try {
+    const step_name = core.getInput('matrix-step-name');
+    const matrix_key = core.getInput('matrix-key');
+    const outputs = core.getInput('outputs');
+
+    core.debug("step_name:")
+    core.debug(step_name)
+
+    core.debug("matrix_key:")
+    core.debug(matrix_key)
+
+    core.debug("outputs:")
+    core.debug(outputs)
+
+    function isEmptyInput(value) {
+        return value === null || value.trim() === "";
+    }
+
+    if (isEmptyInput(step_name) && !isEmptyInput(matrix_key)) {
+        core.setFailed("`matrix-step-name` can not be empty when `matrix-key` specified");
+        return
+    }
+
+    if (!isEmptyInput(step_name) && isEmptyInput(matrix_key)) {
+        core.setFailed("`matrix-key` can not be empty when `matrix-step-name` specified");
+        return
+    }
+
+    if (!isEmptyInput(outputs)) {
+        try {
+            yaml.parse(outputs)
+        }
+        catch (error) {
+            message = `Outputs should be valid YAML 
+---------------------
+${outputs}
+---------------------
+${error}`;
+            core.setFailed(message);
+            return
+        }
+    }
+
+    const outputs_struct = !isEmptyInput(outputs) ? yaml.parse(outputs) : {}
+
+    Object.keys(outputs_struct).forEach(function(key, index) {
+        core.setOutput(key, outputs_struct[key])
+    });
+
+    core.debug("outputs_struct:")
+    core.debug(outputs_struct)
+
+    core.debug("JSON.stringify(outputs_struct):")
+    core.debug(JSON.stringify(outputs_struct))
+
+    core.setOutput('result', JSON.stringify(outputs_struct))
+
+    if (!isEmptyInput(outputs)) {
+        const artifact_content = isEmptyInput(matrix_key) ? outputs_struct : { matrix_key: outputs_struct }
+
+        fs.writeFileSync("./" + step_name, JSON.stringify(artifact_content));
+        const fileBuffer = fs.readFileSync("./" + step_name);
+        const hashSum = crypto.createHash('sha256');
+        hashSum.update(fileBuffer);
+
+        const hex = hashSum.digest('hex');
+
+        const artifactClient = artifact.create()
+        const artifactName = hex;
+        const files = [
+            "./" + step_name,
+        ]
+
+        const rootDirectory = '.' // Also possible to use __dirname
+        const options = {
+            continueOnError: false
+        }
+
+        const uploadResponse = artifactClient.uploadArtifact(artifactName, files, rootDirectory, options)
+    }
+} catch (error) {
+    core.setFailed(error.message);
+}
